bbf993086140a027857e58aed389cb692d153857
sbk0716 <uhey0716@gmail.com>
2022-09-18 09:34:06 +0900

    add terraform-aws-modules/vpc/aws

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,26 @@
+# ================================================================================
+# terraform-aws-modules/vpc/aws
+# Terraform module which creates VPC resources on AWS
+# 
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+# ================================================================================
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  # version = "3.14.4"
+
+  name = "k8s-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true     # Use one NAT GW to reduce costs.
+  enable_vpn_gateway = false    # VPN GW is not necessary
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
